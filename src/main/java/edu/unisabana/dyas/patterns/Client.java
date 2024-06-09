ccb3153bb23c0dc4098ec9829606870be98369a4
oscardiazs <diazsanchezoscar@gmail.com>
2024-06-08 21:11:02 -0400

    Adaptadator completado

--- src/main/java/edu/unisabana/dyas/patterns/Client.java
+++ src/main/java/edu/unisabana/dyas/patterns/Client.java
@@ -1,12 +1,18 @@
 package edu.unisabana.dyas.patterns;
 
 import edu.unisabana.dyas.patterns.utils.AdvancedAudioPlayer;
+import edu.unisabana.dyas.patterns.utils.AdvancedAudioPlayerAdapter;
+import edu.unisabana.dyas.patterns.utils.AudioPlayer;
 
 // Cliente que utiliza directamente AdvancedAudioPlayer sin conocer la interfaz AudioPlayer
 public class Client {
     public static void main(String[] args) {
-        AdvancedAudioPlayer advancedPlayer = new AdvancedAudioPlayer();
-        advancedPlayer.playMp4("video.mp4");
-        advancedPlayer.stop();
+        AudioPlayer audioPlayer = new AdvancedAudioPlayerAdapter(new AdvancedAudioPlayer());
+        audioPlayer.play("mp4", "video.mp4");
+        audioPlayer.stop();
+        
+        // AdvancedAudioPlayer advancedPlayer = new AdvancedAudioPlayer();
+        // advancedPlayer.playMp4("video.mp4");
+        // advancedPlayer.stop();
     }
 }
--- src/main/java/edu/unisabana/dyas/patterns/utils/AdvancedAudioPlayerAdapter.java
+++ src/main/java/edu/unisabana/dyas/patterns/utils/AdvancedAudioPlayerAdapter.java
@@ -1,16 +1,27 @@
 package edu.unisabana.dyas.patterns.utils;
 
 // Clase que implementa una interfaz diferente
-public class AdvancedAudioPlayer {
-    public void playMp4(String fileName) {
-        System.out.println("Reproduciendo archivo MP4: " + fileName);
+public class AdvancedAudioPlayerAdapter implements AudioPlayer {
+
+    private AdvancedAudioPlayer advancedAudioPlayer;
+
+    public AdvancedAudioPlayerAdapter(AdvancedAudioPlayer advancedAudioPlayer) {
+        this.advancedAudioPlayer = advancedAudioPlayer;
     }
 
-    public void playVlc(String fileName) {
-        System.out.println("Reproduciendo archivo VLC: " + fileName);
+    @Override
+    public void play(String audioType, String fileName) {
+        if (audioType.equalsIgnoreCase("mp4")) {
+            advancedAudioPlayer.playMp4(fileName);
+        } else if (audioType.equalsIgnoreCase("vlc")) {
+            advancedAudioPlayer.playVlc(fileName);
+        } else {
+            System.out.println("El archivo no es soportado");
+        }
     }
-
     public void stop() {
-        System.out.println("Deteniendo reproducción");
+        advancedAudioPlayer.stop();;
     }
+
+
 }
